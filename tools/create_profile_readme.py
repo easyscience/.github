776c3e0f6e3fa06da59088e8f4bde66adf37453f
Piotr Rozyczko <piotr.rozyczko@gmail.com>
2025-08-28 13:50:06 +0200

    Fix project URL for diffraction-lib
    Updated URL mapping for diffraction-lib to point to the correct repository.

--- tools/create_profile_readme.py
+++ tools/create_profile_readme.py
@@ -122,7 +122,7 @@ def generate_markdown(data: Dict) -> str:
 
     # TEMPORARY FIX: Change some project urls
     urls = {
-        "https://github.com/easyscience/diffraction-lib": "https://github.com/easyscience/EasyDiffractionLib",
+        "https://github.com/easyscience/diffraction-lib": "https://github.com/easyscience/diffraction-lib",
         "https://github.com/easyscience/diffraction-app": "https://github.com/easyscience/EasyDiffractionApp",
         "https://github.com/easyscience/diffraction": "https://github.com/easyscience/EasyDiffraction",
         "https://github.com/easyscience/reflectometry-lib": "https://github.com/easyscience/EasyReflectometryLib",
